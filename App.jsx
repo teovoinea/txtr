fc501e3871462365b2b1750779f7d81f5353cb48
Teo Voinea <voinea.teodor@gmail.com>
2016-09-14 20:39:40 -0400

    react base

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component {
+
+	render(){
+		return(
+			<div> This will eventually have stuff in it </div>
+		);
+	}
+}
+
+export default App;
